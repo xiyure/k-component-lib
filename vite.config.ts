f147095de2cb7e86cd078a12788f142d736d6a43
周韩杰 <zhouhanjie@kingsware.cn>
2024-05-11 14:58:55 +0800

    story单号：.NA 提交人：周韩杰 提交说明：1.组件库打包配置优化

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,20 @@ import dts from 'vite-plugin-dts'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver  } from 'unplugin-vue-components/resolvers';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
+const name = 'kingsware-ui';
 export default defineConfig({
   plugins: [
     vue(),
     dts({
       include: ['./package'],
       tsconfigPath: 'tsconfig.json'
+    }),
+    copyFilePlugin({
+      src: pathsystem.resolve(__dirname, 'package/components/components.d.ts'),
+      dest: pathsystem.resolve(__dirname, `${name}/components/components.d.ts`)
     }),
     AutoImport({
       resolvers: [ElementPlusResolver()],
@@ -22,10 +28,11 @@ export default defineConfig({
     }),
   ],
   build: {
+    outDir: name,
 		lib: {
 			entry: pathsystem.resolve(__dirname, "package/index.ts"),
-			name: "kingsware-ui",
-			fileName: "kingsware-ui",
+			name: name,
+			fileName: "index",
 		},
 		rollupOptions: {
 			external: ["vue"],
@@ -47,3 +54,25 @@ export default defineConfig({
     port: 12580
   }
 });
+
+type copyFilePluginType = {
+  src: string,
+  dest: string
+}
+// 将某些文件直接复制到打包后的目录下
+function copyFilePlugin({src, dest}:copyFilePluginType) {
+  return {
+    name: 'copy-file-plugin',
+    closeBundle() {
+      try {
+        fs.copyFile(src, dest, (err) => {
+          if (!err) {
+            console.log('successfully');
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  };
+}
