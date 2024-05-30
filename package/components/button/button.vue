45ae58fbeb488f8d7c47aeb7775a4a089342f68c
陆贵顺 <lugs@kingsware.cn>
2024-05-30 14:45:49 +0800

    更新:1.button样式; 2.button添加具名插槽

--- package/components/button/button.vue
+++ package/components/button/button.vue
@@ -1,24 +1,25 @@
 <template>
-  <div
-    :class="{'button-loading': props.loading}"
-    class="k-button"
-  >
+  <div :class="{ 'button-loading': props.loading }" class="k-button">
     <el-button
       ref="buttonRef"
       class="k-button__inner"
-      :class="{'loading': props.loading}"
+      :class="{ loading: props.loading }"
       v-bind="attrs"
       @click="handleClick"
     >
-      <span>
-        <span v-if="props.iconLeft"><props.iconLeft class="icon-left" /></span>
-        <span v-if="props.value">{{ props.value }}</span>
-        <slot
-          v-else
-          class="slot-content"
-        ></slot>
-        <span v-if="props.iconRight"><props.iconRight class="icon-right" /></span>
-      </span>
+      <slot name="iconLeft" class="icon-left">
+        <span v-if="props.iconLeft">
+          <props.iconLeft class="icon-left" />
+        </span>
+      </slot>
+
+      <span v-if="props.value">{{ props.value }}</span>
+      <label v-else><slot class="slot-content"></slot></label>
+      <slot name="iconRight" class="icon-right">
+        <span v-if="props.iconRight">
+          <props.iconRight class="icon-right" />
+        </span>
+      </slot>
     </el-button>
   </div>
 </template>
@@ -26,66 +27,101 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { ButtonProps } from './type';
-import { getCompSize } from '../../utils';
+import { getCompSize } from '../../utils'; // 获取组件大小
+import { IconLoading } from "ksw-vue-icon";
+import "ksw-vue-icon/styles/icon.css";
+
 
 defineOptions({
-  name: 'KButton'
+  name: "KButton",
 });
 
-const props = withDefaults(defineProps<ButtonProps>(), {
-  type: 'normal',
-  size: 'base',
-  iconLeft: null,
-  iconRight: null,
-  value: '',
+const props = withDefaults(defineProps<IButtonProps>(), {
+  type: "",
+  size: "base",
+  value: "",
   disabled: false,
   loading: false,
-  loadingIcon: undefined,
+  loadingIcon: IconLoading,
+  iconLeft: null,
+  iconRight: null,
 });
 
-const attrs = computed(() => ({
-  ...getBtnTypeAttrs(props.type),
-  ...getOriginAttrs()
-}));
+console.log(props.loadingIcon.props.size);
 
-const getBtnTypeAttrs = (type: string):object => {
+props.loadingIcon.props.spin = {
+  type: Boolean,
+  default: true,
+};
+props.loadingIcon.props.color = {
+  type: String,
+  default: "#2882ff", //currentColor
+};
+
+const attrs = computed(() => {
+  const { type, size } = props;
+  return {
+    ...getBtnTypeAttrs(type),
+    ...getSizeAttrs(size),
+    ...getOriginAttrs(),
+  };
+});
+
+const getBtnTypeAttrs = (type: string): object => {
   const typeAttrs = {
-    type: '',
+    type: "",
     plain: false,
-    text: false
+    text: false,
+    iconLeft: null,
+    iconRight: null,
   };
   switch (type) {
-    case 'main':
-      typeAttrs.type = 'primary';
+    case "":
+      typeAttrs.type = "";
       break;
-    case 'secondary':
-      typeAttrs.type = 'primary';
-      typeAttrs.plain = true;
+    case "main":
+      typeAttrs.type = "main";
       break;
-    case 'normal':
-      typeAttrs.type = '';
+    case "secondary":
+      typeAttrs.type = "secondary";
       break;
-    case 'text': 
-      typeAttrs.type = 'primary';
-      typeAttrs.text = true;
+    case "text":
+      typeAttrs.type = "text";
+      break;
+    case "icon":
+      typeAttrs.type = "icon";
       break;
   }
   return typeAttrs;
 };
 
+const getSizeAttrs = (size: string): object => {
+  const sizeAttrs = {
+    size: "default",
+  };
+  switch (size) {
+    case "base":
+      sizeAttrs.size = "";
+      break;
+    case "sm":
+      sizeAttrs.size = "sm";
+      break;
+  }
+  return sizeAttrs;
+};
+
 const getOriginAttrs = () => {
-  const { size, disabled, loading, loadingIcon } = props;
+  const { disabled, loading, loadingIcon } = props;
   return {
-    size: getCompSize(size),
     disabled,
     loading,
     loadingIcon,
   };
 };
 
-const emits = defineEmits(['click']);
-const handleClick = (e:Event) => {
-  emits('click', e);
+const emits = defineEmits(["click"]);
+const handleClick = (e: Event) => {
+  emits("click", e);
 };
 </script>
 
