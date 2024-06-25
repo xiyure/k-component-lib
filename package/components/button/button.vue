bdb07516b2b749ca208c282dfd5f1a387e69267e
baobaomi900901 <baobaomi900901@icloud.com>
2024-06-25 22:59:49 +0800

    change: button

--- package/components/button/button.vue
+++ package/components/button/button.vue
@@ -23,7 +23,7 @@
         <props.iconLeft class="icon-left" />
       </span>
     </slot>
-    <span v-if="props.value">{{ props.value }}</span>
+    <label v-if="props.value">{{ props.value }}</label>
     <label v-else><slot class="slot-content"></slot></label>
     <slot name="iconRight" class="icon-right">
       <span v-if="props.iconRight">
@@ -34,28 +34,28 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, onMounted } from 'vue';
-import { IconLoading } from 'ksw-vue-icon';
-import { ButtonProps } from './type.d';
-import { isValidColor, GetColorLevel } from '../../utils';
-import 'ksw-vue-icon/styles/icon.css';
+import { computed, ref, onMounted } from "vue";
+import { IconLoading } from "ksw-vue-icon";
+import { ButtonProps } from "./type.d";
+import { isValidColor, GetColorLevel } from "../../utils";
+import "ksw-vue-icon/styles/icon.css";
 
 defineOptions({
-  name: 'KButton',
+  name: "KButton",
 });
 
 const buttonRef = ref();
 
 const props = withDefaults(defineProps<ButtonProps>(), {
-  type: 'normal',
-  size: 'base',
-  value: '',
+  type: "normal",
+  size: "base",
+  value: "",
   disabled: false,
   loading: false,
   loadingIcon: IconLoading,
   iconLeft: null,
   iconRight: null,
-  color: '',
+  color: "",
 });
 
 onMounted(() => {
@@ -63,11 +63,25 @@ onMounted(() => {
   if (props.color && isValidColor(props.color)) {
     const hexColor = props.color;
     const { lightColor, darkColor } = GetColorLevel(hexColor, 0.1);
-    // 添加一个 css 颜色变量
-    buttonRef.value.style.setProperty('--k-button-color', hexColor);
-    buttonRef.value.style.setProperty('--k-button-hover-color', lightColor);
-    buttonRef.value.style.setProperty('--k-button-active-color', darkColor);
-    buttonRef.value.style.setProperty('--k-button-icon-color', hexColor);
+    // TODO: 这里为了 storybook 而添加的，后续需要删除
+    if (buttonRef.value.style) {
+      // 添加一个 css 颜色变量
+      buttonRef.value.style.setProperty("--k-button-color", hexColor);
+      buttonRef.value.style.setProperty("--k-button-hover-color", lightColor);
+      buttonRef.value.style.setProperty("--k-button-active-color", darkColor);
+      buttonRef.value.style.setProperty("--k-button-icon-color", hexColor);
+    } else if (buttonRef.value.ref.style) {
+      buttonRef.value.ref.style.setProperty("--k-button-color", hexColor);
+      buttonRef.value.ref.style.setProperty(
+        "--k-button-hover-color",
+        lightColor
+      );
+      buttonRef.value.ref.style.setProperty(
+        "--k-button-active-color",
+        darkColor
+      );
+      buttonRef.value.ref.style.setProperty("--k-button-icon-color", hexColor);
+    }
   }
 });
 
@@ -83,11 +97,13 @@ const getOriginAttrs = () => {
   };
 };
 
-const getSizeClass = computed(() => (props.size !== '' ? `el-button--${ props.size }` : ''));
+const getSizeClass = computed(() =>
+  props.size !== "" ? `el-button--${props.size}` : ""
+);
 
-const emits = defineEmits(['click']);
+const emits = defineEmits(["click"]);
 const handleClick = (e: Event) => {
-  emits('click', e);
+  emits("click", e);
 };
 </script>
 
