47286e87707674cdff4d0d15d776aa3ce35e6188
baobaomi900901 <baobaomi900901@icloud.com>
2024-06-29 17:38:20 +0800

    change: button > type.d

--- package/components/button/type.d.ts
+++ package/components/button/type.d.ts
@@ -3,35 +3,35 @@ import { Component } from 'vue';
 
 export interface ButtonProps {
   /**
-   *   继承 el-button 的颜色
+   *   继承 el-button 的颜色, 可以配合 main, secondary, text 使用
    */
   type?: 'primary' | 'success' | 'info' | 'warning' | 'danger' | '';
-    /**
-   * 主要按钮
+  /**
+   * 主要按钮, main, secondary, text, icon 不能同时使用
    */
-  main?: boolean,
+  main?: boolean;
   /**
    * 次要按钮
    */
-  secondary?: boolean,
+  secondary?: boolean;
   /**
    * 文字按钮
    */
-  text?: boolean,
+  text?: boolean;
   /**
    * icon按钮
    */
-  icon?: boolean,
+  icon?: boolean;
   /**
    * 按钮尺寸，可选值为:
    */
   size?: 'base' | 'sm';
   /**
-   * 文字左边图标
+   * 文字左边图标. 例如：'iconAdd'
    */
   iconLeft?: string;
   /**
-   * 文字右边图标
+   * 文字右边图标, 例如：'IconArrowBottom'
    */
   iconRight?: string;
   /**
