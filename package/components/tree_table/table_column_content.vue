98203cc6c3c8d3f6da85b8361b947d651b605100
周韩杰 <zhouhanjie@kingsware.cn>
2024-07-17 10:41:12 +0800

    1.tree-table组件表格列使用图标时支持对文本内容的格式化；

--- package/components/tree_table/table_column_content.vue
+++ package/components/tree_table/table_column_content.vue
@@ -11,7 +11,10 @@
       :color="row.__folder ? '#FAC814' : row.iconStyle?.empty ? '#cdcacf' : row.iconStyle?.color"
       :size="row.iconStyle?.size ?? (props.size ==='mini'? 16 : 20)"
     />
-    {{ row[col.field] === '' ? '-' : row[col.field] ?? '-' }}
+    <template v-if="typeof col.formatter === 'function'">
+      {{ col.formatter({cellValue: row[col.field], row, column:col}) }}
+    </template>
+    <template v-else>{{ row[col.field] === '' ? '-' : row[col.field] ?? '-' }}</template>
   </span>
 </template>
 
