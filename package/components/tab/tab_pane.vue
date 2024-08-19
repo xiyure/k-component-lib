eadffb3358ee8c014d535bf41e1abc125a9dacd9
baobaomi900901 <baobaomi900901@icloud.com>
2024-08-19 11:38:13 +0800

    change: tab 下划线长度优化

--- package/components/tab/tab_pane.vue
+++ package/components/tab/tab_pane.vue
@@ -1,15 +1,30 @@
 <template>
-  <el-tab-pane class="k-tab-item" v-bind="$attrs">
-    <template v-if="$slots.label" #label>
-      <slot name="label"></slot>
+  <el-tab-pane class="k-tab-item" v-bind="$attrs" :closable="props.closable">
+    <template #label>
+      <span title="label" :class="props.closable ? 'k-tab-item-closeable' : ''">
+        <slot name="label">{{ props.label }}</slot>
+      </span>
     </template>
     <slot></slot>
   </el-tab-pane>
 </template>
 
 <script setup lang="ts">
+const props = defineProps({
+  closable: {
+    type: Boolean,
+    default: false,
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+});
+
+console.log(props);
+
 defineOptions({
-  name: 'KTabPane'
+  name: 'KTabPane',
 });
 </script>
 
