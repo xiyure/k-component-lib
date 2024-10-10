81e932aa0e4b8d25e616d6d98a977e009779f457
sengoku-f <31064651+sengoku-f@users.noreply.github.com>
2024-10-10 16:20:58 +0800

    添加 Steps 步骤条文档

---
+++ docs/docs/components/Navigation/Steps/centered.vue
@@ -0,0 +1,7 @@
+<template>
+  <k-steps style="max-width: 600px" :active="2" align-center>
+    <k-step title="Step 1" description="Some description" />
+    <k-step title="Step 2" description="Some description" />
+    <k-step title="Step 3" description="Some description" />
+  </k-steps>
+</template>
