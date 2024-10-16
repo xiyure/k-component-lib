98f62440f0cf0e79ceab18e427597b1bfb9304bd
moby <baobaomi900901@icloud.com>
2024-10-16 17:52:53 +0800

    优化filter_form组件样式

--- package/components/filter_form/filter_form.vue
+++ package/components/filter_form/filter_form.vue
@@ -1,6 +1,7 @@
 <template>
   <k-form
     ref="KFormRef"
+    class="filtr-items"
     :model="formData"
     :size="size"
     :class="['k-form', _styleModule]"
@@ -9,6 +10,7 @@
       v-for="item in items"
       :key="item.prop"
       v-bind="item"
+      :style="`grid-column: span ${item.column}`"
     >
       <slot :name="item.prop" :form-data="formData">
         <!-- 自定义render -->
@@ -24,7 +26,7 @@
         ></k-input>
         <!-- select类型 -->
         <k-select
-          v-else-if="item.type ==='select'"
+          v-else-if="item.type === 'select'"
           v-model="formData[item.prop]"
           v-bind="item.attrs"
           :size="compSize(item.attrs)"
@@ -51,13 +53,16 @@
           >
             <k-checkbox
               v-for="option in item.options"
-              v-bind="option" :key="option"
+              v-bind="option"
+              :key="option"
               :size="compSize(item.attrs)"
             ></k-checkbox>
           </k-checkbox-group>
           <k-checkbox
-            v-else v-for="option in item.options"
-            v-bind="option" :key="option"
+            v-else
+            v-for="option in item.options"
+            v-bind="option"
+            :key="option"
             v-model="formData[item.prop]"
             :size="compSize(item.attrs)"
           ></k-checkbox>
@@ -72,12 +77,16 @@
         </template>
       </slot>
     </k-form-item>
-    <k-form-item>
+
+    <div class="filtr-btns flex gap-2" ref="filtrBtns">
       <slot name="action">
         <k-button :size="compSize()" @click="reset">{{ $t('reset') }}</k-button>
         <k-button :size="compSize()" @click="search" main>{{ $t('query') }}</k-button>
+        <KButton text :iconRight="handleExpandBtnIcon" @click="handleExpand">
+          {{ handleExpandBtnText }}
+        </KButton>
       </slot>
-    </k-form-item>
+    </div>
   </k-form>
 </template>
 
@@ -94,18 +103,33 @@ import { FilterFormProps } from './type';
 import { getExposeProxy } from '../../utils';
 
 defineOptions({
-  name: 'KFilterForm'
+  name: 'KFilterForm',
 });
+
+const filtrBtns = ref();
+const KFormRef = ref();
+
+const handleExpandBtnText = ref('展开');
+const handleExpandBtnIcon = ref('IconArrowBottom');
+
+const handleExpand = () => {
+  console.log(1, KFormRef.value.$el);
+
+  filtrBtns?.value.classList.toggle('is-expand');
+  KFormRef?.value.$el.classList.toggle('is-expand');
+  handleExpandBtnText.value = handleExpandBtnText.value === '展开' ? '收起' : '展开';
+  handleExpandBtnIcon.value =
+    handleExpandBtnIcon.value === 'IconArrowBottom' ? 'IconArrowTop' : 'IconArrowBottom';
+};
 
 const DEFAULT_SIZES = ['base', 'sm'];
 const _styleModule = inject('_styleModule', '');
 const props = withDefaults(defineProps<FilterFormProps>(), {
   items: () => [],
-  size: 'base'
+  size: 'base',
 });
 
 const emits = defineEmits(['search', 'reset', 'change']);
-const KFormRef = ref();
 const formData = ref({});
 
 const compSize = computed(() => (attrs: any = {}) => {
@@ -116,16 +140,23 @@ const compSize = computed(() => (attrs: any = {}) => {
   return props.size;
 });
 
-watch(() => props.items, () => {
-  props.items.forEach((item: any) => {
-    formData.value[item.prop] = item.value;
-  });
-}, { deep: true, immediate: true });
+watch(
+  () => props.items,
+  () => {
+    props.items.forEach((item: any) => {
+      formData.value[item.prop] = item.value;
+    });
+  },
+  { deep: true, immediate: true },
+);
 
-watch(() => formData.value, (newValue: any) => {
-  emits('change', newValue);
-}, { deep: true })
-
+watch(
+  () => formData.value,
+  (newValue: any) => {
+    emits('change', newValue);
+  },
+  { deep: true },
+);
 
 function search() {
   emits('search', formData.value);
@@ -139,11 +170,46 @@ function reset() {
   emits('reset');
 }
 
+function getFormData() {
+  return formData.value;
+}
+
 // expose instance
-const instance: any = { reset };
+const instance: any = { reset, getFormData, search };
 defineExpose(getExposeProxy(instance, KFormRef));
 </script>
 
 <style lang="less">
 @import './style.less';
+
+.filtr-items {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 1rem;
+  // grid-auto-rows: 1fr;
+  overflow: hidden;
+  height: 2rem;
+  transition: all 0.3s ease-in-out;
+  &.is-expand {
+    height: auto;
+    transition: all 0.3s ease-in-out;
+  }
+}
+
+.filtr-btns {
+  // width: 30%;
+  // 让元素在第一行 最后一个
+  grid-column: 3 / 4;
+  grid-row: 1 / 2;
+  justify-self: end;
+  &.is-expand {
+    grid-row: unset;
+  }
+}
+
+.column-2 {
+  // 元素占据两个 grid 单元
+  grid-column: span 2;
+}
 </style>
