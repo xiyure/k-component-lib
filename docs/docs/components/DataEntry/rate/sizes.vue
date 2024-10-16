b8c36ae74ad8f7688fdd13223142bfaa116774dc
sengoku-f <31064651+sengoku-f@users.noreply.github.com>
2024-10-16 17:09:27 +0800

    更新 Rate 评分文档

---
+++ docs/docs/components/DataEntry/rate/sizes.vue
@@ -0,0 +1,13 @@
+<template>
+  <k-rate v-model="value" size="large" />
+  <br />
+  <k-rate v-model="value" />
+  <br />
+  <k-rate v-model="value" size="small" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const value = ref(0)
+</script>
---
+++ docs/docs/components/DataEntry/rate/text-usage.vue
@@ -0,0 +1,13 @@
+<template>
+  <k-rate
+    v-model="value"
+    :texts="['糟糕', '失望', '正常', '好', '太棒了']"
+    show-text
+  />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const value = ref()
+</script>
