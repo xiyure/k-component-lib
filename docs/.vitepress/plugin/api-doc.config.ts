8a24f328812c65358d723bc78e21cc6d90130810
周韩杰 <zhouhanjie@kingsware.cn>
2024-10-28 14:42:54 +0800

    1.组件库文档新增参数版本标识； 2.tree-table组件文档部分参数添加version属性；

--- docs/.vitepress/plugin/api-doc.config.ts
+++ docs/.vitepress/plugin/api-doc.config.ts
@@ -1,7 +1,7 @@
 import { resolveLocaleConfigs, parseProps } from './utils.js';
 import MarkdownIt from 'markdown-it';
 import matter from 'gray-matter';
-import { dirname, join, resolve } from 'node:path';
+import { dirname, resolve } from 'node:path';
 import fs from 'fs';
 import { componentProps, Props, Events, Methods, Slots } from './type.js';
 
@@ -31,39 +31,25 @@ function cleanStr(str: string) {
 // 生成Props模板
 const tmplProps = (props: Props[]) => {
   const displayableProps = props;
-  const hasVersion = displayableProps.some((prop) => prop?.tags?.version);
   const content = displayableProps.map((prop) => {
-    const { displayName, description, type, defaultValue, tip } = escapeParams(prop);
-    let lineContent = `|${displayName}|${description}|\`${type}\`${tip}|\`${defaultValue}\`|`;
-    if (hasVersion) {
-      const version = prop?.tags?.version?.[0]?.description;
-      lineContent += `${version || ""}|`;
-    }
+    const { displayName, description, type, defaultValue, tip, version } = escapeParams(prop);
+    let lineContent = `|${displayName} ${genTag(version)}|${description}|\`${type}\`${tip}|\`${defaultValue}\`|`;
     return lineContent;
   }).join("\n");
   return {
-    hasVersion,
     content
   };
 };
 // 处理Props
 const handleProps = (props: Props[], lang: Lang = 'zh') => {
-  const { content, hasVersion } = tmplProps(props);
+  const { content } = tmplProps(props);
   if (!content)
     return "";
   let header = ['', ''];
   if (lang === 'en') {
     header = ["|Name|Description|Type|Default|", "|---|---|---|:---:|"];
-    if (hasVersion) {
-      header[0] += "version|";
-      header[1] += ":---|";
-    }
   } else {
     header = ["|\u53C2\u6570\u540D|\u63CF\u8FF0|\u7C7B\u578B|\u9ED8\u8BA4\u503C|", "|---|---|---|:---:|"];
-    if (hasVersion) {
-      header[0] += "\u7248\u672C|";
-      header[1] += ":---|";
-    }
   }
   return `
 ${header[0]}
@@ -75,42 +61,21 @@ ${content}
 // 生成Events模板
 const tmplEvents = (events: Events[]) => {
   const displayableEvents = events;
-  const hasVersion = displayableEvents.some(
-    (event) => event?.tags?.some((tag: any) => tag.title === "version")
-  );
   const content = displayableEvents.map((event) => {
-    const { tags } = event;
-    const { displayName, description, type, tip } =  escapeParams(event);
-    let version = '';
-    if (tags?.length) {
-      for (const item of tags) {
-        if (item.title === "version") {
-          version = item.content;
-          continue;
-        }
-      }
-    }
-    let lineContent = `|${displayName}|${description}|\`${type}\`${tip}|`;
-    if (hasVersion) {
-      lineContent += `${version}|`;
-    }
+    const { displayName, description, type, tip, version } =  escapeParams(event);
+    let lineContent = `|${displayName} ${genTag(version)}|${description}|\`${type}\`${tip}|`;
     return lineContent;
   }).join("\n");
   return {
-    content,
-    hasVersion
+    content
   };
 };
 // 处理Events
 const handleEvents = (events: Events[], lang: Lang = 'zh') => {
-  const { content, hasVersion } = tmplEvents(events);
+  const { content } = tmplEvents(events);
   if (!content)
     return "";
   const header = lang === "en" ? ["|Event Name|Description|Type|", "|---|---|---|"] : ["|\u4E8B\u4EF6\u540D|\u63CF\u8FF0|\u7c7b\u578b|", "|---|---|---|"];
-  if (hasVersion) {
-    header[0] += lang === "en" ? "version|" : "\u7248\u672C|";
-    header[1] += ":---|";
-  }
   return `
 ${header[0]}
 ${header[1]}
@@ -121,32 +86,21 @@ ${content}
 // 生成Methods模板
 const tmplMethods = (methods: Methods[]) => {
   const displayableMethods = methods;
-  const hasVersion = displayableMethods.some((method) => method?.tags?.version);
   const content = displayableMethods.map((method) => {
-    const { tags } = method;
-    const { displayName, description, type, tip } =  escapeParams(method);
-    let lineContent = `|${displayName}|${description}|\`${type}\`${tip}|`;
-    if (hasVersion) {
-      const version = tags?.version?.[0]?.description;
-      lineContent += `${version || ""}|`;
-    }
+    const { displayName, description, type, tip, version } =  escapeParams(method);
+    let lineContent = `|${displayName} ${genTag(version)}|${description}|\`${type}\`${tip}|`;
     return lineContent;
   }).join("\n");
   return {
-    hasVersion,
     content
   };
 };
 // 处理Methods
 const handleMethods = (methods: Methods[], lang: Lang = 'zh') => {
-  const { content, hasVersion } = tmplMethods(methods);
+  const { content } = tmplMethods(methods);
   if (!content)
     return "";
   const header = lang === "en" ? ["|Method|Description|Type|", "|---|---|---|"] : ["|\u65B9\u6CD5\u540D|\u63CF\u8FF0|\u7c7b\u578b|", "|---|---|---|"];
-  if (hasVersion) {
-    header[0] += lang === "en" ? "version|" : "\u7248\u672C|";
-    header[1] += ":---|";
-  }
   return `
 ${header[0]}
 ${header[1]}
@@ -157,32 +111,21 @@ ${content}
 // 生成Slots模板
 const tmplSlots = (slots: Slots[]) => {
   const displayableSlots = slots;
-  const hasVersion = displayableSlots.some((slot) => slot?.tags?.version);
   const content = displayableSlots.map((slot) => {
-    const { tags } = slot;
-    const { displayName, description, parameters, tip } =  escapeParams(slot);
-    let lineContent = `|${displayName}|${description}|\`${parameters}\`${tip}|`;
-    if (hasVersion) {
-      const version = tags?.version?.content;
-      lineContent += `${version || ""}|`;
-    }
+    const { displayName, description, parameters, tip, version } =  escapeParams(slot);
+    let lineContent = `|${displayName} ${genTag(version)}|${description}|\`${parameters}\`${tip}|`;
     return lineContent;
   }).join("\n");
   return {
-    hasVersion,
     content
   };
 };
 // 处理Slots
 const handleSlots = (slots: Slots[], lang: Lang = 'zh') => {
-  const { content, hasVersion } = tmplSlots(slots);
+  const { content } = tmplSlots(slots);
   if (!content)
     return "";
   const header = lang === "en" ? ["|Slot Name|Description|Parameters|", "|---|---|---|"] : ["|\u63D2\u69FD\u540D|\u63CF\u8FF0|\u53C2\u6570|", "|---|---|---|"];
-  if (hasVersion) {
-    header[0] += lang === "en" ? "version|" : "\u7248\u672C|";
-    header[1] += ":---|";
-  }
   return `
 ${header[0]}
 ${header[1]}
@@ -191,25 +134,16 @@ ${content}
 };
 
 const getTmpl = (suffix: string, content: string, options: any) => {
-  const { name, tags, lang } = options;
-  if (!content)
-    return "";
-  let title = `${name ?? ''} ${suffix}`;
-  if (tags?.version) {
-    const version = tags.version[0]?.description;
-    version && (title += ` (${version})`);
+  const { name } = options;
+  if (!content) {
+    return '';
   }
-  let description = "";
-  if (suffix === "Props" && tags?.[lang]) {
-    description = tags[lang][0]?.description;
-  }
-  return `### ${title}${description ? `
-${description}` : ""}
-${content}`;
+  return `### ${name ?? ''} ${suffix}
+  ${content}`;
 };
 const getApiTmpl = (componentDoc: any, lang: Lang) => {
-  const { name, props, subProps = [], events, methods, slots, tags } = componentDoc;
-  const options = { name, tags, lang };
+  const { name, props, subProps = [], events, methods, slots } = componentDoc;
+  const options = { name, lang };
   const subPropsTmpl: any[] = [];
   for (const key in subProps) {
     const subProp = getTmpl('' ,handleProps(subProps[key], lang), { ...options, name: key});
@@ -284,22 +218,30 @@ async function getApiMarkdown(apiComponent: any, localeConfigs: any, baseDir: st
   return apiMdContents || `${srcPath}'s api is empty!`;
 }
 
-function genTooltip(content: any) {
+function genTooltip(content: string) {
   if (!content) {
     return '';
   }
   const html = `<KTooltip content="${content.toString()}" effect="light" trigger="click"></KTooltip>`;
   return html;
 }
+function genTag(content: string) {
+  if (!content) {
+    return '';
+  }
+  const html = `<KTag type="primary" size="sm" text-color="#2882ff" style="background-color: #FFF" round>${content.toString()}</KTag>`;
+  return html;
+}
 function escapeParams(params: any) {
-  const { displayName, description, type, defaultValue, parameters, tip } = params
+  const { displayName, description, type, defaultValue, parameters, tip, version } = params
   return {
     displayName: escapeCharacter(displayName),
     description: escapeCharacter(description),
     type: escapeCharacter(type),
     defaultValue: escapeCharacter(defaultValue + '') || '-',
     tip: escapeCharacter(genTooltip(tip ?? parameters)),
-    parameters: parameters ? 'object' : '-' 
+    parameters: parameters ? 'object' : '-' ,
+    version: escapeCharacter(version)
   }
 }
 
