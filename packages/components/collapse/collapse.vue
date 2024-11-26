a05af0f0296862e70c879e3198f6eca219959ecc
莫启健 <qijian_mo@163.com>
2024-11-26 16:15:01 +0800

    collapse组件新增isOutline参数控制是否清除所有描边

--- packages/components/collapse/collapse.vue
+++ packages/components/collapse/collapse.vue
@@ -1,9 +1,11 @@
 <template>
-  <el-collapse ref="kCollapseRef" :class="['k-collapse', _styleModule]" v-bind="$attrs">
-    <template v-for="(_, name) in $slots" :key="name" #[name]="data">
-      <slot :name="name" v-bind="data"></slot>
-    </template>
-  </el-collapse>
+  <div :class="{ 'k-collapse-outline': isOutline }">
+    <el-collapse ref="kCollapseRef" :class="['k-collapse', _styleModule]" v-bind="$attrs">
+      <template v-for="(_, name) in $slots" :key="name" #[name]="data">
+        <slot :name="name" v-bind="data"></slot>
+      </template>
+    </el-collapse>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -12,19 +14,23 @@ import { ElCollapse } from 'element-plus';
 import { getExposeProxy } from '../../utils/index';
 
 defineOptions({
-  name: 'KCollapse'
+  name: 'KCollapse',
 });
 
 const props = defineProps({
   block: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
+  isOutline: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 provide(
   '__isBlock__',
-  computed(() => props.block)
+  computed(() => props.block),
 );
 
 const _styleModule = inject('_styleModule', '');
