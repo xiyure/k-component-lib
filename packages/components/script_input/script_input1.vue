b2ea58e397c939db1fd2129c896ab860c3a9cb69
唐清伟 <tangqingwei@kingsware.cn>
2024-11-28 10:10:42 +0800

    优化样式

--- packages/components/script_input/script_input1.vue
+++ packages/components/script_input/script_input1.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="k-script-input-wrapper">
+  <div class="k-script-input-wrapper" ref="KScriptInputWrapper">
     <div class="k-script-input-prepend">
       <slot name="prepend"></slot>
     </div>
     <k-popover
-      width="100%"
+      :width="popover"
       :show-arrow="false"
       :visible="popperVisible"
       popper-class="k-script-input-popper"
       @show="onShowPopper"
       @hide="onHidePopper"
+      placement="bottom"
     >
       <template #reference>
         <div
@@ -20,13 +21,17 @@
           @input="handleInput"
           @blur="handleBlur"
           @focus="handleFocus"
-          @compositionstart="() =>{
-            isAllowInput = false;
-          }"
-          @compositionend="(e: InputEvent) => {
-            isAllowInput = true;
-            handleInput(e)
-          }"
+          @compositionstart="
+            () => {
+              isAllowInput = false;
+            }
+          "
+          @compositionend="
+            (e: InputEvent) => {
+              isAllowInput = true;
+              handleInput(e);
+            }
+          "
         ></div>
       </template>
       <k-tree-table
@@ -47,15 +52,18 @@
           keyField: 'value',
           isCurrent: true,
           currentMethod: ({ row }) => {
-          return row.optional !== false
-        }}"
-        :row-class-name="({ row }) => {
-          if (row.optional === false && !row.children?.length) {
-            return 'k-script-input-tree-disabled';
-          }
-          return '';
+            return row.optional !== false;
+          },
         }"
-        :tree-config="{parentField: 'pid', rowField: 'value'}"
+        :row-class-name="
+          ({ row }) => {
+            if (row.optional === false && !row.children?.length) {
+              return 'k-script-input-tree-disabled';
+            }
+            return '';
+          }
+        "
+        :tree-config="{ parentField: 'pid', rowField: 'value' }"
         highlight-current
         adaptive
         @cell-click="cellClick"
@@ -81,14 +89,13 @@ const _styleModule = inject('_styleModule', '');
 const props = withDefaults(defineProps<ScriptInputProps>(), {
   options: () => [],
   size: 'base',
-  useTree: false
+  useTree: false,
 });
 
 const emits = defineEmits(['change', 'input', 'focus', 'blur', 'select', 'update:modelValue']);
 
 onMounted(() => {
   document.addEventListener('keydown', toggleSelect);
- 
 });
 onBeforeUnmount(() => {
   document.removeEventListener('keydown', toggleSelect);
@@ -104,48 +111,85 @@ const curInput = ref('');
 const textValue = ref('');
 const selectedIndex = ref<number>(0);
 const popperVisible = ref(false);
-const columns = [
-  { field: 'label', label: '', treeNode: true }
-]
+const columns = [{ field: 'label', label: '', treeNode: true }];
+
+const isShowInput = ref(false);
+
+const KScriptInputWrapper = ref();
+const popover = ref();
+
+// 获取 KScriptInputWrapper 的宽度
+const getWidth = () => {
+  nextTick(() => {
+    if (!KScriptInputWrapper.value) {
+      return;
+    }
+    const { width } = KScriptInputWrapper.value.getBoundingClientRect();
+    popover.value = `${width}px`;
+  });
+};
+
+// 监听屏幕尺寸发生改变时
+watch(
+  () => window.innerWidth,
+  () => {
+    getWidth();
+  },
+  {
+    immediate: true,
+  },
+);
 
 const flattedOptions = computed(() => {
-  const tableData = $tree.value?.getTableData().fullData?? [];
+  const tableData = $tree.value?.getTableData().fullData ?? [];
   return allTreeDataToArray(tableData, 'children') ?? [];
 });
 
-watch(() => curInput.value, () => {
-  if (curInput.value === '') {
-    popperVisible.value = false;
-    return;
-  }
-  $tree.value?.filter(curInput.value);
-  if ($tree.value?.getVisibleData()?.length) {
-    popperVisible.value = true;
-  } else {
-    popperVisible.value = false;
-  }
-}, { immediate: true })
-watch(() => props.modelValue, (newValue) => {
-  const type = typeof newValue;
-  if ((type !== 'string' && type !== 'number') || newValue === undefined) {
-    console.warn(`'modelValue' must be a string or number, but got ${type}`);
-    return;
-  }
-  if (newValue === cacheRes) {
-    return;
-  }
-  nextTick(() => {
-    clear();
-    textValue.value = props.modelValue.toString();
-    preTextValue = textValue.value;
-  });
-}, {immediate: true})
+watch(
+  () => curInput.value,
+  () => {
+    if (curInput.value === '') {
+      popperVisible.value = false;
+      return;
+    }
+    $tree.value?.filter(curInput.value);
+    if ($tree.value?.getVisibleData()?.length) {
+      popperVisible.value = true;
+    } else {
+      popperVisible.value = false;
+    }
+  },
+  { immediate: true },
+);
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    const type = typeof newValue;
+    if ((type !== 'string' && type !== 'number') || newValue === undefined) {
+      console.warn(`'modelValue' must be a string or number, but got ${type}`);
+      return;
+    }
+    if (newValue === cacheRes) {
+      return;
+    }
+    nextTick(() => {
+      clear();
+      textValue.value = props.modelValue.toString();
+      preTextValue = textValue.value;
+    });
+  },
+  { immediate: true },
+);
 
-watch(() => textValue.value, () => {
-  const res = parseText();
-  cacheRes = res;
-  emits('update:modelValue', res);
-}, {immediate: true})
+watch(
+  () => textValue.value,
+  () => {
+    const res = parseText();
+    cacheRes = res;
+    emits('update:modelValue', res);
+  },
+  { immediate: true },
+);
 
 function handleInput(event: InputEvent) {
   if (!(event.target instanceof HTMLElement) || !isAllowInput.value) {
@@ -153,7 +197,7 @@ function handleInput(event: InputEvent) {
   }
   if (event.data === ' ') {
     curInput.value = '';
-  } else if(event.data === null) {
+  } else if (event.data === null) {
     curInput.value = curInput.value.slice(0, -1);
   } else {
     curInput.value += event.data ?? '';
@@ -164,13 +208,13 @@ function handleFocus() {
   emits('focus');
 }
 function handleBlur() {
-  if (preTextValue!== textValue.value) {
+  if (preTextValue !== textValue.value) {
     preTextValue = textValue.value;
     parseText();
   }
   emits('blur');
 }
-function cellClick({ row }: { row: any}) {
+function cellClick({ row }: { row: any }) {
   if (row.optional === false) {
     return;
   }
@@ -185,7 +229,7 @@ function selectOption(data: any) {
   let endIndex = curInput.value.length ? -curInput.value.length : undefined;
   textValue.value = textValue.value.slice(0, endIndex);
   curInput.value = '';
-  textValue.value += generateScriptTag(data.label)
+  textValue.value += generateScriptTag(data.label);
   preTextValue = textValue.value;
   KScriptInput.value.innerHTML = textValue.value;
   cursorToEnd();
@@ -194,13 +238,14 @@ function selectOption(data: any) {
 }
 function parseText() {
   let text = textValue.value;
-  const replaceReg = /<div[^>]*>(.*?)<\/div>/
+  const replaceReg = /<div[^>]*>(.*?)<\/div>/;
   while (replaceReg.test(text)) {
     const regex = />([\s\S]*?)<\/div>/;
     const label = text.match(regex)?.[1] ?? '';
     text = text.replace(replaceReg, `${prefix}-${label}&nbsp;`);
   }
-  const res = text.split('&nbsp;')
+  const res = text
+    .split('&nbsp;')
     .filter((item) => item !== '')
     .map((item) => {
       if (item.startsWith(prefix + '-')) {
@@ -215,21 +260,23 @@ function parseText() {
   return res;
 }
 function generateScriptTag(content: string) {
-  return `<div class="k-script-tag" contenteditable="false">${content}</div>`
+  return `<div class="k-script-tag" contenteditable="false">${content}</div>`;
 }
 function toggleSelect(event: KeyboardEvent) {
   const dataLength = flattedOptions.value.length;
   if (event.code === 'ArrowUp') {
     selectedIndex.value = (selectedIndex.value - 1 + dataLength) % dataLength;
-    while (isHideNode(flattedOptions.value[selectedIndex.value])
-      || flattedOptions.value[selectedIndex.value].optional === false
+    while (
+      isHideNode(flattedOptions.value[selectedIndex.value]) ||
+      flattedOptions.value[selectedIndex.value].optional === false
     ) {
       selectedIndex.value = (selectedIndex.value - 1 + dataLength) % dataLength;
     }
   } else if (event.code === 'ArrowDown') {
     selectedIndex.value = (selectedIndex.value + 1) % dataLength;
-    while (isHideNode(flattedOptions.value[selectedIndex.value])
-      || flattedOptions.value[selectedIndex.value].optional === false
+    while (
+      isHideNode(flattedOptions.value[selectedIndex.value]) ||
+      flattedOptions.value[selectedIndex.value].optional === false
     ) {
       selectedIndex.value = (selectedIndex.value + 1) % dataLength;
     }
@@ -276,8 +323,10 @@ function onHidePopper() {
   selectedIndex.value = 0;
   $tree.value?.setCurrentRow(null);
   $tree.value?.clearTreeExpand();
+  isShowInput.value = false;
 }
 function showPopper() {
+  isShowInput.value = true;
   popperVisible.value = true;
 }
 function hidePopper() {
@@ -294,8 +343,8 @@ function clear() {
 defineExpose({
   clear,
   showPopper,
-  hidePopper
-})
+  hidePopper,
+});
 </script>
 <style lang="less">
 @import './style.less';
