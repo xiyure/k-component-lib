a68a4a512b54c881c0e0b83ccaa407e757bb4fc1
MoQiJian <qijian_mo@163.com>
2024-12-19 10:14:55 +0800

    菜单视图API文档编写

--- docs/docs/templates/KSW_UI/menuView/def.vue
+++ docs/docs/templates/KSW_UI/menuView/def.vue
@@ -1,12 +1,101 @@
+<!--
+* @description  参数1
+* @fileName  pageTemporarily
+* @author userName
+* @date 2024-11-21 11:52:01
+* @version V3.0.0
+!-->
 <template>
-  <KMenuView :options="treeData"></KMenuView>
+  <div :style="{ height: '100%', overflow: 'auto' }">
+    <KMenuView
+      ref="menuView"
+      :options="viewOptions"
+      default-active="option1"
+    >
+      <template #app-logo>
+        <img class="w-6 h-6" src="/vite.svg" alt="logo" />
+        <span class="ml-2 !text-base text-white text-nowrap">菜单视图模板</span>
+      </template>
+      <template #header></template>
+      <template #main>
+      </template>
+      <template #footer>Footer</template>
+    </KMenuView>
+  </div>
 </template>
 
-<script setup lang="tsx">
-const treeData = [
-  { index: 1, title: '菜单1',icon: 'IconAomMenuGNHSColor', children: [{ index: 11, title: '菜单1-1', children: [] }] },
-  { index: 2, title: '菜单2', children: [], disabled: true },
-  { index: 3, title: '菜单3', children: [] },
+<script lang="tsx" setup>
+
+const viewOptions = [
+  {
+    title: '菜单层级示例(一级)',
+    index: 'option1',
+    icon: 'IconAdd',
+    disabled: false,
+    children: [
+      {
+        title: 'option1-1',
+        index: '',
+        children: [
+          {
+            title: 'Option1-1-1',
+            index: '',
+            disabled: false,
+            icon: 'IconFilter',
+          },
+          {
+            title: 'Option1-1-2',
+            index: '',
+            disabled: false,
+            icon: 'IconRanking',
+          },
+          {
+            title: 'Option1-1-3',
+            index: '',
+            disabled: false,
+            icon: 'IconOrganization',
+          },
+        ],
+        disabled: false,
+      },
+      {
+        title: 'Option1-2',
+        index: '',
+        disabled: false,
+      },
+      {
+        title: 'Option1-3',
+        index: '',
+        disabled: false,
+      },
+    ],
+  },
+  {
+    title: 'cardView',
+    index: 'card-view',
+    disabled: false,
+    icon: 'IconMenuDefColor',
+  },
+  {
+    title: 'tableView',
+    index: 'table-view',
+    disabled: false,
+    icon: 'IconMenuDefColor',
+  },
+  {
+    title: 'detailsView',
+    index: 'details-view',
+    disabled: false,
+    icon: 'IconMenuDefColor',
+  },
+  {
+    title: 'toolMarket',
+    index: 'tool-market',
+    disabled: false,
+    icon: 'IconMenuDefColor',
+  },
 ];
 
 </script>
+
+<style lang="less" scoped></style>
--- docs/docs/templates/KSW_UI/menuView/menuView.md
+++ docs/docs/templates/KSW_UI/menuView/menuView.md
@@ -7,3 +7,5 @@ title: menuView 菜单视图
 ## 示例
 
 <preview path="./def.vue" />
+
+<API src="./data.json" lang="zh"></API>
