b9ba906726b691440996e44c48a642fb7bc84b38
MoQiJian <qijian_mo@163.com>
2024-12-19 10:49:54 +0800

    pageDetailsAPI文档编写

--- docs/docs/templates/KSW_UI/pageDetails/def.vue
+++ docs/docs/templates/KSW_UI/pageDetails/def.vue
@@ -1,12 +1,48 @@
 <template>
-  <k-page-details
-    pageIcon="IconAomMenuDLDXXColor"
-    pageTitle="KSW UI"
-    :abstract="treeData"
-    :showLine="true"
-  ></k-page-details>
+  <div style="height: 500px">
+    <k-page-details
+      page-icon="IconAomMenuAOMRZColor"
+      page-title="功能详情页"
+      page-descriptions="这里是功能详情页的描述信息"
+      :abstract="abstract"
+      class="h-full bg-blue flex flex-col"
+    >
+      <template #head-toolbar>
+        <KButton main>保存</KButton>
+        <KButton>取消</KButton>
+      </template>
+      <template #head-abstract></template>
+
+      <!-- 默认插槽 -->
+      <div class="KPageDetailsBody w-full flex flex-col bg-white rounded mt-3 flex-1">默认插槽</div>
+    </k-page-details>
+  </div>
 </template>
 
-<script setup lang="tsx">
-const treeData = [{ label: '小标题', value: '内容', render: 'IconAomMenuGNHSColor' }];
+<script lang="tsx" setup>
+
+const abstract = [
+  {
+    label: '姓名',
+    value: '张三',
+  },
+  {
+    label: '年龄',
+    value: '25',
+  },
+  {
+    label: '性别',
+    value: '男',
+  },
+  {
+    label: '职业',
+    render: () => (
+      <div class='flex flex-wrap gap-2'>
+        <k-tag>前端开发</k-tag>
+        <k-tag>后端开发</k-tag>
+      </div>
+    ),
+  },
+];
 </script>
+<style lang="less"></style>
--- docs/docs/templates/KSW_UI/pageDetails/pageDetails.md
+++ docs/docs/templates/KSW_UI/pageDetails/pageDetails.md
@@ -7,3 +7,5 @@ title: pageDetails 页面详情
 ## 示例
 
 <preview path="./def.vue" />
+
+<API src="./data.json" lang="zh"></API>
