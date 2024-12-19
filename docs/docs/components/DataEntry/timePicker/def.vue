f48b27844c7bde241457f98d04ef5a3c37fe8533
MoQiJian <qijian_mo@163.com>
2024-12-19 11:40:08 +0800

    完成timePicker文档编写

---
+++ docs/docs/components/DataEntry/timePicker/def.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="example-basic">
+    <k-time-picker class="k-date-editor" v-model="value1" placeholder="Arbitrary time" />
+    <k-time-picker
+      class="k-date-editor"
+      v-model="value2"
+      arrow-control
+      placeholder="Arbitrary time"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const value1 = ref();
+const value2 = ref();
+</script>
+
+<style>
+.example-basic .k-date-editor {
+  margin: 8px;
+}
+</style>
---
+++ docs/docs/components/DataEntry/timePicker/rangeTimePicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="demo-range">
+    <k-time-picker
+      class="k-date-editor"
+      v-model="value1"
+      is-range
+      range-separator="To"
+      start-placeholder="Start time"
+      end-placeholder="End time"
+    />
+    <k-time-picker
+      class="k-range-separator"
+      v-model="value2"
+      is-range
+      arrow-control
+      range-separator="To"
+      start-placeholder="Start time"
+      end-placeholder="End time"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const value1 = ref<[Date, Date]>([new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)]);
+const value2 = ref<[Date, Date]>([new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)]);
+</script>
+
+<style>
+.demo-range .k-date-editor {
+  margin: 8px;
+}
+
+.demo-range .k-range-separator {
+  box-sizing: content-box;
+}
+</style>
