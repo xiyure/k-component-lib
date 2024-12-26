d61e39f88f26cc11f2f2ffe55d042d122545189e
mobytang <tangqingwei@kingsware.cn>
2024-12-26 22:18:02 +0800

    修复 form label样式

--- packages/components/form/form.vue
+++ packages/components/form/form.vue
@@ -12,14 +12,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref, inject, provide, computed } from 'vue';
+import { ref, inject, provide, computed, onMounted, nextTick } from 'vue';
 import { ElForm, FormItemInstance } from 'element-plus';
 import { FormProps } from './type';
 import { getExposeProxy, SIZE_KEY } from '../../utils';
 import { useSize } from '../../hooks';
 
 defineOptions({
-  name: 'KForm'
+  name: 'KForm',
 });
 
 const props = withDefaults(defineProps<FormProps>(), {});
@@ -33,11 +33,32 @@ const KFormRef = ref<FormItemInstance>();
 const instance: any = {};
 defineExpose(getExposeProxy(instance, KFormRef));
 
+nextTick(() => {
+  // 获取所有 el-form-item__label 的内容
+  const labels = document.querySelectorAll('.el-form-item__label');
+
+  for (let i = 0; i < labels.length; i++) {
+    const childNodes = labels[i].childNodes;
+    if (!childNodes || childNodes.length === 0) {
+      continue;
+    }
+
+    for (let j = 0; j < childNodes.length; j++) {
+      const text = childNodes[j].textContent;
+      if (childNodes[j].nodeType === 3 && text) {
+        const span = document.createElement('span');
+        span.textContent = text;
+        labels[i].replaceChild(span, childNodes[j]);
+      }
+    }
+  }
+});
+
 provide(SIZE_KEY, formatSize);
 
 provide(
   '__showColon__',
-  computed(() => props.showColon)
+  computed(() => props.showColon),
 );
 </script>
 
