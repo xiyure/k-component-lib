673a5b707b918ff3696fdeafaa321d6ab0fe8d90
周韩杰 <zhouhanjie@kingsware.cn>
2024-12-27 16:11:41 +0800

    v-ksw-tooltip指令优化，防止元素更新时重复创建多个tooltip实例；

--- packages/directives/tooltip.ts
+++ packages/directives/tooltip.ts
@@ -1,16 +1,26 @@
 import { createApp, DirectiveBinding, Directive } from 'vue';
 import { KTooltip } from '../components/tooltip';
+import { genRandomStr } from '../utils';
+
+const TOOLTIP_MAP = new Map();
 
 export const tooltip: Directive = {
   mounted(el: HTMLElement, binding: DirectiveBinding) {
     createTooltip(el, binding);
+  },
+  updated(el: HTMLElement, binding: DirectiveBinding) {
+    createTooltip(el, binding);
   }
 };
-
 const createTooltip = (el: any, binding: DirectiveBinding) => {
   const elRoot = document.querySelector('#_tooltip_root');
   if (elRoot) {
     elRoot.remove();
+  }
+  const { key = `_${genRandomStr(8)}` } = binding.value;
+  if (TOOLTIP_MAP.has(key)) {
+    TOOLTIP_MAP.get(key)?.unmount();
+    TOOLTIP_MAP.delete(key);
   }
   el._tipRoot = null;
   el._tipApp = null;
@@ -18,7 +28,7 @@ const createTooltip = (el: any, binding: DirectiveBinding) => {
   const _tipRoot = document.createElement('div');
   _tipRoot.id = id;
   _tipRoot.classList.add('_tipRoot');
-  const { trigger, placement, content, showAfter, autoClose } = binding?.value ?? {};
+  const { trigger, placement, content, showAfter, autoClose, visible } = binding?.value ?? {};
   // 通过createApp 创建实例组件
   const _tipApp = createApp(KTooltip, {
     virtualRef: el,
@@ -28,7 +38,8 @@ const createTooltip = (el: any, binding: DirectiveBinding) => {
     placement: placement ?? 'bottom',
     content: typeof binding.value === 'object' ? String(content) : String(binding.value),
     showAfter: showAfter ?? 500,
-    autoClose: autoClose ?? 1000
+    autoClose: autoClose,
+    visible: visible === true ? false : undefined,
   });
   el._tipRoot = _tipRoot;
   el._tipApp = _tipApp;
@@ -37,5 +48,6 @@ const createTooltip = (el: any, binding: DirectiveBinding) => {
   // 将新组件挂载到根元素
   if (_tipApp && _tipRoot) {
     el._tipApp.mount(`#${id}`);
+    TOOLTIP_MAP.set(key, _tipApp);
   }
 };
