3d47acdcc087af0d58af7db00ab1ec8d22acbe0b
周韩杰 <zhouhanjie@kingsware.cn>
2024-12-27 17:17:34 +0800

    1.v-ksw-tooltip指令优化; 2.tree-table组件API更新；

--- packages/directives/tooltip.ts
+++ packages/directives/tooltip.ts
@@ -1,34 +1,21 @@
-import { createApp, DirectiveBinding, Directive } from 'vue';
+import { createApp, DirectiveBinding, Directive, App } from 'vue';
 import { KTooltip } from '../components/tooltip';
-import { genRandomStr } from '../utils';
 
-const TOOLTIP_MAP = new Map();
+let GLOBAL_TOOL_TIP: App | null = null;
+const TOOL_TIP_ID = '_tooltip_root';
 
-export const tooltip: Directive = {
-  mounted(el: HTMLElement, binding: DirectiveBinding) {
-    createTooltip(el, binding);
-  },
-  updated(el: HTMLElement, binding: DirectiveBinding) {
-    createTooltip(el, binding);
-  }
-};
 const createTooltip = (el: any, binding: DirectiveBinding) => {
   const elRoot = document.querySelector('#_tooltip_root');
   if (elRoot) {
     elRoot.remove();
   }
-  const { key = `_${genRandomStr(8)}` } = binding.value;
-  if (TOOLTIP_MAP.has(key)) {
-    TOOLTIP_MAP.get(key)?.unmount();
-    TOOLTIP_MAP.delete(key);
-  }
   el._tipRoot = null;
   el._tipApp = null;
-  const id = '_tooltip_root';
   const _tipRoot = document.createElement('div');
-  _tipRoot.id = id;
+  _tipRoot.id = TOOL_TIP_ID;
   _tipRoot.classList.add('_tipRoot');
-  const { trigger, placement, content, showAfter, autoClose, visible } = binding?.value ?? {};
+  const { trigger, placement, content, showAfter, autoClose } = binding?.value ?? {};
+  const showContent = typeof binding.value === 'object' ? content : binding.value;
   // 通过createApp 创建实例组件
   const _tipApp = createApp(KTooltip, {
     virtualRef: el,
@@ -36,10 +23,10 @@ const createTooltip = (el: any, binding: DirectiveBinding) => {
     virtualTriggering: true,
     trigger: trigger ?? 'hover',
     placement: placement ?? 'bottom',
-    content: typeof binding.value === 'object' ? String(content) : String(binding.value),
+    content: showContent ?? el.textContent,
     showAfter: showAfter ?? 500,
     autoClose: autoClose,
-    visible: visible === true ? false : undefined,
+    visible: true,
   });
   el._tipRoot = _tipRoot;
   el._tipApp = _tipApp;
@@ -47,7 +34,20 @@ const createTooltip = (el: any, binding: DirectiveBinding) => {
   document.body.appendChild(_tipRoot);
   // 将新组件挂载到根元素
   if (_tipApp && _tipRoot) {
-    el._tipApp.mount(`#${id}`);
-    TOOLTIP_MAP.set(key, _tipApp);
+    el._tipApp.mount(`#${TOOL_TIP_ID}`);
+    GLOBAL_TOOL_TIP = el._tipApp;
+  }
+};
+export const tooltip: Directive = {
+  mounted(el: HTMLElement, binding: DirectiveBinding) {
+    el.addEventListener('mouseenter', () => {
+      createTooltip(el, binding);
+    });
+    el.addEventListener('mouseleave', () => {
+      if (GLOBAL_TOOL_TIP) {
+        GLOBAL_TOOL_TIP?.unmount();
+        GLOBAL_TOOL_TIP = null;
+      }
+    })
   }
 };
