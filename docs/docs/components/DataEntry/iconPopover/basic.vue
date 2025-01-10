af2465b218b47eefa6020d64ccfc8a6aedecd1ef
QijianMo <moqijian@kingsware.cn>
2025-01-10 14:45:28 +0800

    完成iconPopover组件文档和API

---
+++ docs/docs/components/DataEntry/iconPopover/basic.vue
@@ -0,0 +1,29 @@
+<template>
+  <k-icon-popover @updateIcon="handleUpdate">
+    <div class="icon-box">
+      <component v-if="searchContent" :is="searchContent" />
+    </div>
+  </k-icon-popover>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let searchContent = ref('');
+const handleUpdate = (copyContent: string) => {
+  searchContent.value = copyContent;
+};
+</script>
+
+<style lang="less">
+.icon-box {
+  width: 100px;
+  height: 100px;
+  background-color: black;
+  .ksw-icon svg {
+    width: 100px;
+    height: 100px;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ docs/docs/components/DataEntry/iconPopover/iconPopover.md
@@ -0,0 +1,13 @@
+---
+title: iconPopover 图标气泡框
+---
+
+# iconPopover 图标气泡框
+
+## 基础示例
+
+<preview path="./basic.vue" />
+
+## API
+
+<API src="./iconPopover.json" lang="zh"></API>
