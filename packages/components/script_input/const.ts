bc9a08f3e7a00e9c0588df02032c6dbb085a6034
moby <baobaomi900901@icloud.com>
2025-01-15 15:08:43 +0800

    fix(script_input): 修改提示文字

--- packages/components/script_input/const.ts
+++ packages/components/script_input/const.ts
@@ -1,12 +1,16 @@
 export const checkInputMessage = {
-  string: '必须输入字符串',
+  string: '请按照变量规则命名',
   boolean: '必须输入布尔值',
   number: '必须输入数字',
-  tooltip: '1.名称只能是中文、英文、数字和下划线。<br/>2.名称不能以数字开头。<br>3.名称不能仅由下划线和数字组成。'
-}
+  tooltip:
+    '1.名称只能是中文、英文、数字和下划线。<br/>2.名称不能以数字开头。<br>3.名称不能仅由下划线和数字组成。',
+};
 
 export const typeRules = new Map([
-  ['string', { reg: /^(?!\d)(?![_\d]+$)[\u4e00-\u9fa5A-Za-z0-9_]+$/, message: checkInputMessage.string }],
+  [
+    'string',
+    { reg: /^(?!\d)(?![_\d]+$)[\u4e00-\u9fa5A-Za-z0-9_]+$/, message: checkInputMessage.string },
+  ],
   ['number', { reg: /^-?\d+(\.\d+)?$/, message: checkInputMessage.number }],
-  ['boolean', { reg: /^(true|false)$/, message: checkInputMessage.boolean }]
-])
+  ['boolean', { reg: /^(true|false)$/, message: checkInputMessage.boolean }],
+]);
