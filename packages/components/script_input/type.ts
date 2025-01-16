d88a9d3172e2964b473fe8a3cded5a9e470b3fe4
mobytang <tangqingwei@kingsware.cn>
2025-01-16 21:31:42 +0800

    增加备注

--- packages/components/script_input/type.ts
+++ packages/components/script_input/type.ts
@@ -1,31 +1,40 @@
 export interface ScriptInputProps {
-  modelValue: string | number
-  size?: 'lg' | 'base' | 'sm' | ''
-  placeholder?: string
-  disabled?: boolean
-  options?: ScriptOptions[]
-  useTree?: boolean
-  showPopperSwitch?: boolean
-  showModeSwitch?: boolean
-  defaultMode?: 'string' | 'expression'
-  height?: string
-  onlyOneInput?: boolean
-  resize?: boolean
-  showPassword?: boolean
+  modelValue: string | number;
+  size?: 'lg' | 'base' | 'sm' | '';
+  placeholder?: string;
+  disabled?: boolean;
+  options?: ScriptOptions[];
+  useTree?: boolean;
+  showPopperSwitch?: boolean;
+  showModeSwitch?: boolean;
+  defaultMode?: 'string' | 'expression';
+  height?: string;
+  onlyOneInput?: boolean;
+  resize?: boolean;
+  showPassword?: boolean;
   treeConfig?: {
-    [key: string]: any
-  }
+    [key: string]: any;
+  };
   props?: {
-    label?: string
-    value?: string
-    disabled?: string
-  }
-  scriptKey?: string
-  checkContentType?: boolean
-  contentType?: 'number' | 'boolean' | 'string' | ''
-  optionRepeatable?: boolean
+    label?: string;
+    value?: string;
+    disabled?: string;
+  };
+  scriptKey?: string;
+  /**
+   *   校验输入字符串是否合规, 单独使用时, 作用是校验变量是否合规
+   */
+  checkContentType?: boolean;
+  /**
+   *   校验输入字符串是否合规, number判断是否为数字, 包含浮点; boolean判断是否为true/false;
+   */
+  contentType?: 'number' | 'boolean' | 'string' | '';
+  /**
+   *   值是否可重复, 默认值为true; 改为false, 则值不可重复
+   */
+  optionRepeatable?: boolean;
 }
 
 export type ScriptOptions = {
-  [key: string]: any
-}
+  [key: string]: any;
+};
