034e2254f928c529461eff3eb5bf26dc5827e198
QijianMo <moqijian@kingsware.cn>
2025-02-12 11:16:42 +0800

    新增globalProvider组件文档

---
+++ docs/docs/components/General/globalProvider/def.vue
@@ -0,0 +1,51 @@
+<template>
+  <k-button @click="toggle">Global Provider</k-button>
+  <k-global-provider :locale="locale">
+    <k-tree-table :data="tableData" :column="column"></k-tree-table>
+  </k-global-provider>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const zhTableData = [
+  {
+    id: '1',
+    name: '测试1',
+  },
+  {
+    id: '2',
+    name: '测试2'
+  }
+];
+
+const enTableData = [
+  {
+    id: '1',
+    name: 'Test1',
+  },
+  {
+    id: '2',
+    name: 'Test2'
+  }
+];
+
+let locale = ref('en');
+let tableData = ref(enTableData)
+
+const toggle = () => {
+  locale.value = locale.value === 'en' ? 'zh' : 'en';
+  tableData.value = locale.value === 'en' ? enTableData : zhTableData;
+}
+
+const column = ref([
+  {
+    title: 'Id',
+    field: 'id'
+  },{
+    title: 'Name',
+    field: 'name'
+  }
+]);
+
+</script>
+<style scoped></style>
---
+++ docs/docs/components/General/globalProvider/globalProvider.md
@@ -0,0 +1,15 @@
+---
+title: GlobalProvider 全局配置
+---
+
+# GlobalProvider 全局配置
+
+通过 GlobalProvider 来配置多语言，让你的应用可以随时切换语言。
+
+## 基础用法
+
+<preview path="./def.vue" />
+
+## API
+
+<API src="./globalProvider.json" lang="zh"></API>
