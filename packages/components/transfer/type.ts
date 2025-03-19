17bd9aa64f9ae7570c6ec4e4d29383be067424b2
莫启健 <moqijian@kingsware.cn>
2025-03-19 12:48:36 +0800

    transfer组件type去除多余键值对

--- packages/components/transfer/type.ts
+++ packages/components/transfer/type.ts
@@ -1,27 +1,17 @@
 import { Component } from 'vue';
 
 export interface TransferProps {
-  modelValue: Array<string | number>;
-  data: any[];
-  filterablePlaceholder?: string;
+  modelValue: Array<string | number>
+  data: any[]
+  filterablePlaceholder?: string
   props?: {
-    label?: string;
-    key?: string;
-    disabled?: string;
-  };
-  drag?: boolean;
-  defaultKeys?: (string | number)[];
-  filterable?: boolean;
+    label?: string
+    key?: string
+    disabled?: string
+  }
+  defaultKeys?: (string | number)[]
+  filterable?: boolean
   searchStrictly?: boolean;
-  searchConfig?: TransferSearchConfig;
-  dragIcon?: string | Component;
-}
-
-export interface TransferSearchConfig {
-  strict?: boolean;
-  searchMethod?: (query: string, item: Record<string, any>) => boolean;
-  isRemoteQuery?: boolean;
-  ignoreCase?: boolean;
-  searchColumns?: string[];
-  supportPinYin?: string[] | boolean;
+  drag?: boolean
+  dragIcon?: string | Component
 }
