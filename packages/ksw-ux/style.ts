4f79fe7a59074ef3433f0cd299c6fa1197b1d644
莫启健 <moqijian@kingsware.cn>
2025-03-26 15:27:14 +0800

    公共样式表更换引入位置，去除无意义引入

--- packages/ksw-ux/style.ts
+++ packages/ksw-ux/style.ts
@@ -3,11 +3,12 @@ import type { ContextConfig } from '../utils/typescript';
 import { mergeCssVar, setStyleTheme } from '../utils';
 import '../style/elStyle/index.css';
 import 'vxe-table/lib/style.css';
+import '../variable.css';
+import '../general.less';
 import 'ksw-vue-icon/styles/icon.css';
 import '../style/tailwind.css';
 import 'overlayscrollbars/styles/overlayscrollbars.css';
 import '../style/theme/index.css';
-import '../style/global.less';
 
 // style of lib
 export function registerStyle(_app: App, config: ContextConfig = {}) {
